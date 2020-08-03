0679bd3ff59a1b65e6442e79903ef4a3c64657bc
Jessy Pinchon <jessy@apsulis.fr>
2020-08-03 17:20:10 +0200

    add form for sign in + styles

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,9 @@
 import React, { Component } from 'react'
 
+import FormInput from '../form-input/form-input.component'
+
+import './sign-in.styles.scss';
+
 export default class SignIn extends Component {
   constructor(props){
     super(props);
@@ -29,22 +33,22 @@ export default class SignIn extends Component {
         <span>Sign in with your email and password</span>
 
         <form onSubmit={this.handleSubmit}>
-          <input 
+          <FormInput 
             name='email' 
             type='email' 
             value={this.state.email}
-            onChange={this.handleChange} 
+            handleChange={this.handleChange} 
+            label='email'
             required 
           />
-          <label>Email</label>
-          <input 
+          <FormInput 
             name='password' 
             type='password' 
             value={this.state.password}
-            onChange={this.handleChange} 
+            handleChange={this.handleChange} 
+            label='password'
             required 
           />
-          <label>Password</label>
           <input type='submit' value='Submit form' />
         </form>
       </div>
